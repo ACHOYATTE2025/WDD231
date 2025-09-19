4ea4c686a0ef8eb6261a736220eafab2f9f596f3
unknown <acho.quebec@gmail.com>
2025-09-19 22:07:11 +0000

    chamber

--- chamber/scripts/chamber.js
+++ chamber/scripts/chamber.js
@@ -1,4 +1,5 @@
 /* hambuger and navbar menu */
+const navButton = document.querySelector('#nav-button');
 
 const navBar = document.querySelector('#nav-bar');
 const cards = document.querySelector('#insider');
