0bd9c255418839957eca406a990b8e2fd8b6b854
unknown <acho.quebec@gmail.com>
2025-10-08 16:12:21 +0000

    finalproject

--- finalproject/scripts/footer.mjs
+++ finalproject/scripts/footer.mjs
@@ -8,7 +8,8 @@ export  function footershow() {
   
   const today = new Date();
   let year = today.getFullYear();
-  const exoTime= document.querySelector("#currentyear");
+  const exoTime= document.getElementById('lastModified');
+  const exoyear =document.getElementById('currentyear');
   
 
   const yearN = today.getFullYear();
@@ -19,21 +20,13 @@ export  function footershow() {
   const minutes = today.getMinutes();
   const seconds = today.getSeconds();
 
-  //Alépé information and contact
-  const Chambertop = document.querySelector('#chamber_top');
-  const infoChamber = document.querySelector('#chamber_text');
-  const author = document.querySelector('#author');
-  Chambertop.innerHTML=`<p><strong>Alépé Chamber of Commerce</strong> `;
-  infoChamber.innerHTML = `<p> Rue principale Danguira<br>
-                              Alépé ,Côte D'Ivoire 692<br>
-                              info@alepe.org<br>
-                              (225) 07 48 36 56 19</p>`;
 
   // Format personnalisé (ex: 01/07/2025 22:45:00)
-  author.innerHTML=`<p>WDD231 Class Project<br>Yatte Deivy Constant ACHO<br>
-                      &copy;${year} |Alépé Chamber of Commerce <br>
-                      Last modification : ${month}/${day}/${year} ${hours}:${minutes}:${seconds}</p>`;
-        
+  exoTime.innerHTML=`<p> Last modification : ${month}/${day}/${year} ${hours}:${minutes}:${seconds}</p>`;
+
+                      
+  exoyear.innerHTML=`</p>&copy;${year} <span>CÔTE D'IVOIRE TOURISM</span> — 
+  Designed by <strong>Yatté Deivy Constant ACHO</strong></p>`;
 };
 
 
